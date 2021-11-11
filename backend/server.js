25443640b9f57bfa18992043fd6489d4cedc0091
Kurt Opel <kurt.opel@sogeti.com>
2021-11-11 14:28:47 -0500

    updated for docker network

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const routes = require("./routes");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/todos", {
+  await mongoose.connect("mongodb://mongo:27017/todos", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });

6a45ca6faa11b61c0fa93aec62a32315f80f5bbf
Kurt Opel <kurt.opel@sogeti.com>
2021-11-12 16:01:01 -0500

    cleanup unneeded routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import Home from "./components/home";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
-      <Router>
-        <div>HEADER</div>
-        <div>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-          </Routes>
-        </div>
-      </Router>
+      <Home />
     </div>
   );
 }
